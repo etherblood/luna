b3cbb3b179ab32757305886b309d5c0af232bdd7
etherblood <philipp@jobach.de>
2022-03-30 22:01:19 +0200

    placeholder background & tile

--- releng/deploy.sh
+++ releng/deploy.sh
@@ -7,6 +7,8 @@ CLIENT=$3
 cp ${GIT}/application-client/target/application-client-*[!s].jar ${CLIENT}/luna.jar
 rsync -rc --delete ${GIT}/application-client/target/assets ${CLIENT}/
 rsync -rc --delete ${GIT}/application-client/target/libs ${CLIENT}/
+cp ${GIT}/releng/background.png /var/www/destrostudios/launcher/images/background_10.png
+cp ${GIT}/releng/tile.png /var/www/destrostudios/launcher/images/tile_10.png
 curl https://destrostudios.com:8080/apps/10/updateFiles
 echo updated client files
 
